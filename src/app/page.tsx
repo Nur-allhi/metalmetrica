edc8bdd5bfe1d374ec86d204fb9ca7ed1401c1f3
Nur E Allhi <noorefty1@gmail.com>
2025-08-21 18:44:41 +0000

    When user open the app in default show the single Calc

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -152,7 +152,7 @@ export default function Home() {
               <SidebarInset>
                   <Header />
                   <main className="flex flex-1 flex-col gap-4 p-4 sm:p-4 md:p-6 overflow-hidden">
-                      <Tabs defaultValue="projects" className="flex-1 flex flex-col overflow-hidden h-full">
+                      <Tabs defaultValue="single" className="flex-1 flex flex-col overflow-hidden h-full">
                         <div className="flex items-center">
                             <SidebarTrigger className="md:hidden" />
                             <TabsList>

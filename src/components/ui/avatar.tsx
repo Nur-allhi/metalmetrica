19d4c46b49babfb499736322892775071a36e8ec
Nur E Allhi <noorefty1@gmail.com>
2025-08-21 14:36:04 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <Primitive.span cl

--- src/components/ui/avatar.tsx
+++ src/components/ui/avatar.tsx
@@ -39,7 +39,7 @@ const AvatarFallback = React.forwardRef<
   <AvatarPrimitive.Fallback
     ref={ref}
     className={cn(
-      "flex h-full w-full items-center justify-center rounded-full bg-muted",
+      "flex h-full w-full items-center justify-center rounded-full bg-muted border border-input group-hover:bg-accent group-hover:text-accent-foreground transition-colors",
       className
     )}
     {...props}
